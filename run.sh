ad6d51032697ebd49126b2233ad6d178f963bc22
Syntox32 <syntox32@gmail.com>
2015-06-13 21:03:42 +0200

    run.sh now cleans up before proceeding

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
+
+# kills previous jobs if they are lingering around
+pkill -f "python alarm.py"
+
+# run in the background so we can exit the sessions
+# without disrupting the process
 nohup python alarm.py --config alarm.config &
+
+# any 'direct' console output can be read by typing 
+# 'cat nohup.out' in the same directory
